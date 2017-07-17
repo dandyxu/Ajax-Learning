4c54f4952f6c2249feb28f8a5a32c84ad5e7399b
dandyxu <dandyjefferson@gmail.com>
2017-07-17 10:18:01 +0100

    Initial commit and Basic examples

---
+++ ajax_html/new_content.php
@@ -0,0 +1,9 @@
+<?php
+    $var = "Hello, this is the Hello World of Ajax Learning.";
+
+    // tricky debug skill
+//    echo $var;
+//    return;
+?>
+
+This is the <?php echo $var; ?> which for Respond with HTML partials lesson.
