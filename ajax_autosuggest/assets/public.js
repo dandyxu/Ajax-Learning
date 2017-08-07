e9b2c06a22cb14ad4c7fda2d4dd9089639a8f380
dandyxu <dandyjefferson@gmail.com>
2017-08-07 17:20:44 +0100

    Search AutoSuggest: Style suggestions and Send an AutoSuggest Ajax Request

--- ajax_autosuggest/assets/public.js
+++ ajax_autosuggest/assets/public.js
@@ -8,10 +8,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function showSuggestions(json) {
 
+        suggestions.style.display = 'block';
+
     }
 
     function getSuggestions() {
         var q = search.value;
+
+        // console.log('getSuggestions');
+        // return;
+
+        if (q.length < 3) {
+            suggestions.style.display = 'none';
+            return;
+        }
 
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'autosuggest.php?q=' + q, true);
@@ -20,6 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 var result = xhr.responseText;
                 console.log('Results: ' + result);
+                result = '{}';
 
                 var json = JSON.parse(result);
                 showSuggestions(json);
@@ -28,6 +39,6 @@ document.addEventListener('DOMContentLoaded', function() {
         xhr.send();
     }
 
-    // search.addEventListener("", getSuggestions, false);
+    search.addEventListener("input", getSuggestions);
 
 });
